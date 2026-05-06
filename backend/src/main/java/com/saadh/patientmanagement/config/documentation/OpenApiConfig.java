1cac06a774d0fcac51659d29d99da797e63af250
itz_me_saadh <itzmesaadh@gmail.com>
2026-05-06 15:16:34 +0530

    Added Cors Config

--- backend/src/main/java/com/saadh/patientmanagement/config/documentation/OpenApiConfig.java
+++ backend/src/main/java/com/saadh/patientmanagement/config/documentation/OpenApiConfig.java
@@ -1,4 +1,4 @@
-package com.saadh.patientmanagement.config;
+package com.saadh.patientmanagement.config.documentation;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
